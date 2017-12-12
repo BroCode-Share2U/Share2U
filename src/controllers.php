dbcf7a8f4c0d4b0c5f0dda9aaa471760c20ea97e
Cyril Giuliani <giuliani.cyril@gmail.com>
2017-12-12 20:50:36 +0100

    Create all route and methods

--- src/controllers.php
+++ src/controllers.php
@@ -8,12 +8,89 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array());
-})
-->bind('homepage')
-;
+/* *********************
+* Default Controller
+********************* */
+/* homepage */
+$app->get('/', 'Controller\DefaultController::homepageAction')
+    ->bind('homepage');
+/* support */
+$app->get('/support', 'Controller\DefaultController::supportAction')
+    ->bind('support');
+/* dashboard */
+$app->get('/dashboard', 'Controller\DefaultController::dashboardAction')
+    ->bind('dashboard');
 
+/* *********************
+* Comment Controller
+********************* */
+/* comment */
+$app->match('/comment/{loanId}', 'Controller\CommentController::commentAction')
+    ->bind('comment');
+
+/* *********************
+* User Controller
+********************* */
+/* profile */
+$app->get('/profile/{username}', 'Controller\UserController::showAction')
+    ->bind('profile');
+/* profile edit */
+$app->match('/profile/edit', 'Controller\UserController::editAction')
+    ->bind('profileEdit');
+/* signin */
+$app->get('/signin', 'Controller\UserController::signinAction')
+    ->bind('signin');
+/* signup */
+$app->match('/signup', 'Controller\UserController::signupAction')
+    ->bind('signup');
+/* reset */
+$app->match('/reset', 'Controller\UserController::resetAction')
+    ->bind('reset');
+/* delete */
+$app->delete('/admin/user/{userId}', 'Controller\UserController::deleteAction')
+    ->bind('deleteUser');
+/* admin panel */
+$app->get('/admin/user', 'Controller\UserController::adminPanelAction')
+    ->bind('adminPanel');
+
+/* *********************
+* Item Controller
+********************* */
+/* item */
+$app->get('/item/{itemId}', 'Controller\ItemController::showAction')
+    ->bind('item');
+/* add item */
+$app->match('/item/add', 'Controller\ItemController::addAction')
+    ->bind('addItem');
+/* edit item */
+$app->match('/item/edit/{itemId}', 'Controller\ItemController::editAction')
+    ->bind('editItem');
+/* delete item */
+$app->delete('/item/{itemId}', 'Controller\ItemController::deleteAction')
+    ->bind('deleteItem');
+/* search item */
+$app->get('/search', 'Controller\ItemController::searchAction')
+    ->bind('searchItem');
+
+/* *********************
+* Loan Controller
+********************* */
+/* request loan */
+$app->match('/request/{itemId}', 'Controller\LoanController::requestAction')
+    ->bind('requestItem');
+/* accept loan */
+$app->patch('/loan/accept/{loanId}', 'Controller\LoanController::acceptAction')
+    ->bind('acceptRequest');
+/* reject loan */
+$app->patch('/loan/reject/{loanId}', 'Controller\LoanController::rejectAction')
+    ->bind('rejectRequest');
+/* close loan */
+$app->patch('/loan/close/{loanId}', 'Controller\LoanController::closeAction')
+    ->bind('closeRequest');
+
+/* *********************
+* Error route
+********************* */
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
